d07ba47dfab9da09b06c456d45c5c9e33e345e30
addamb <addamb@gmail.com>
2014-04-27 10:32:06 -0600

    Added the Rest of the Views and Controllers.

---
+++ app/scripts/controllers/initialSetup.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('devAppAApp')
+  .controller('initialSetupCtrl', function ($scope, ngDialog) {   	
+    	$scope.options = [
+		    {name:'Github'},
+		    /*{name:'white'},
+		    {name:'red'},
+		    {name:'blue'},
+		    {name:'yellow'}*/
+		  ];
+		  $scope.option = $scope.options[0]; //Github
+
+		  $scope.closeThisDialog = function() {
+		  	ngDialog.close();
+
+		  }	
+  });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,25 @@
 
 angular.module('devAppAApp')
   .controller('MainCtrl', function ($scope, ngDialog) {
+  	//Initial Setup Popup
+  	$scope.initialSetup = function(){
+  		ngDialog.open({ template: 'views/initialSetup.html'});
+  	}
+
+  	//New Project Popup
     $scope.newProject = function(){
     	ngDialog.open({ template: 'views/createNewProject.html'});	
-		    }
+	}
+
+	//Add Exsisting Popup
+	$scope.addExisting = function(){
+		ngDialog.open({ template: 'views/addExistingProject.html'});
+	}
+
+	//Migrare Popup
+	$scope.migrate = function(){
+		ngDialog.open({ template: 'views/migrate.html'});
+	}
+
+
   });
