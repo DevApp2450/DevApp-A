749e00dc4991be68565189de22d50192d2d4d98c
Daniel Baker <dbaker@sorenson.com>
2014-05-04 15:22:18 -0600

    Updated create new project view.

--- app/scripts/controllers/newProject.js
+++ app/scripts/controllers/newProject.js
@@ -1,8 +1,7 @@
 'use strict';
 
 angular.module('devAppAeApp')
-  .controller('newProjectCtrl', function ($scope, ngDialog) {  
-  $scope.repo = {url: 'http://'};	 	
+  .controller('newProjectCtrl', function ($scope, ngDialog) { 
     	$scope.options = [
 		    {name:'WordPress'},
 		    {name:'GoDaddy'},
@@ -14,8 +13,18 @@ angular.module('devAppAeApp')
 		  ];
 		  $scope.option = $scope.options[0]; //Wordpress
 
+		  $scope.repoOptions = [
+		    {name:'Repo 1'},
+		    {name:'Repo 2'},
+		    {name:'Repo 3'}
+		    /*{name:'white'},
+		    {name:'red'},
+		    {name:'blue'},
+		    {name:'yellow'}*/
+		  ];
+		  $scope.repoOption = $scope.repoOptions[0]; //Repo 1
+
 		  $scope.closeThisDialog = function() {
 		  	ngDialog.close();
-
 		  }	
   });
