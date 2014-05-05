f7a22bd5b28877392965cedd4bb1449ba56695c0
addamb <addamb@gmail.com>
2014-05-05 17:02:19 -0600

    Now retrieving data from database onto initial setup screen.

--- app/scripts/controllers/initialSetup.js
+++ app/scripts/controllers/initialSetup.js
@@ -1,7 +1,20 @@
 'use strict';
 
 angular.module('devAppAeApp')
-  .controller('initialSetupCtrl', function ($scope, ngDialog, $http) {   
+  .controller('initialSetupCtrl', function ($scope, ngDialog, $http) { 
+
+  		//var data = $http.get('/api/getRepos');
+  		//$scope.repoEmail = data.Email;
+  		//console.log($http.get('/api/getRepos')); 
+
+  		$http.get('/api/getRepos').success(function(returnedRepo) {
+  			console.log(returnedRepo);
+  			$scope.repoEmail = returnedRepo.Email;
+  			$scope.repoUserName = returnedRepo.Username;
+  			$scope.repoPassword = returnedRepo.Password;
+  			$scope.repoUrl = returnedRepo.Url;
+  			$scope.repoFolder = returnedRepo.Folder;
+    	}); 
 
 	  	$scope.save = function(){
 	  		var json = {Email: $scope.repoEmail, Username: $scope.repoUserName, Password: $scope.repoPassword, Url: $scope.repoUrl, Folder: $scope.repoFolder};
