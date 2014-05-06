0148528b04c2fc74c0ffe4f708eca26e2cee1264
Anthony Browness <demos74dx@gmail.com>
2014-05-06 00:43:34 -0600

    Finally got a drop down to work.  It's in the Edit Repo page.  There's a small bug where you need to open the Edit Repo page, close it, and then open it again to make the drop down populate.  Addam, any ideas on this?  We still need to create a database update operation to modify the edited data.  The selected option in the drop down does populate the rest of the form.  We can follow this same idea to populate the Projects on the main page.

--- app/scripts/controllers/initialSetup.js
+++ app/scripts/controllers/initialSetup.js
@@ -1,23 +1,25 @@
 'use strict';
 
+var lastId = 0;
+
 angular.module('devAppAeApp')
   .controller('initialSetupCtrl', function ($scope, ngDialog, $http) { 
 
   		//var data = $http.get('/api/getRepos');
   		//$scope.repoEmail = data.Email;
   		//console.log($http.get('/api/getRepos')); 
+    
+    $http.get('/api/getRepos').success(function(returnedRepo) {
+      console.log(returnedRepo);
+      lastId = returnedRepo.length;
+      console.log(lastId);
+    });
 
-  		$http.get('/api/getRepos').success(function(returnedRepo) {
-  			console.log(returnedRepo);
-  			$scope.repoEmail = returnedRepo.Email;
-  			$scope.repoUserName = returnedRepo.Username;
-  			$scope.repoPassword = returnedRepo.Password;
-  			$scope.repoUrl = returnedRepo.Url;
-  			$scope.repoFolder = returnedRepo.Folder;
-    	}); 
 
 	  	$scope.save = function(){
-	  		var json = {Email: $scope.repoEmail, Username: $scope.repoUserName, Password: $scope.repoPassword, Url: $scope.repoUrl, Folder: $scope.repoFolder};
+        var nextId = lastId + 1;
+        console.log(nextId);
+	  		var json = {repoId: nextId, repoEmail: $scope.repoEmail, repoUsername: $scope.repoUserName, repoPassword: $scope.repoPassword, repoUrl: $scope.repoUrl, repoFolder: $scope.repoFolder};
 	  		$http.post('/api/insertRepo', json);
 	  		//Save somthing
 	  		ngDialog.close();
